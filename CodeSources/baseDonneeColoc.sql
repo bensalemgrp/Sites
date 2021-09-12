c4b0b09b3d5a57481fc9fcda1d52b5ac27b29d3d
amira <amirafk6@gmail.com>
2021-09-12 19:34:51 +0100

    modify prf_profiles database

--- CodeSources/baseDonneeColoc.sql
+++ CodeSources/baseDonneeColoc.sql
@@ -168,29 +168,38 @@ INSERT INTO `pay_pays` (`pay_id`, `description`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `prf_profile`
+-- Table structure for table `prf_profiles`
 --
 
-CREATE TABLE `prf_profile` (
-  `prf_id` int(11) NOT NULL,
+CREATE TABLE `prf_profiles` (
+  `prf_id` int(11) UNSIGNED NOT NULL,
   `prf_first_name` varchar(50) NOT NULL,
   `prf_last_name` varchar(50) NOT NULL,
-  `prf_email` datetime(6) DEFAULT NULL,
+  `prf_email` varchar(30) DEFAULT NULL,
   `prf_password` varchar(70) DEFAULT NULL,
+  `id_typeuser` varchar(255) NOT NULL,
+  `prf_logo` varchar(70) DEFAULT NULL,
   `prf_phone` varchar(20) DEFAULT NULL,
-  `prf_adress` varchar(50) NOT NULL,
-  `ville_id` int(11) DEFAULT NULL,
+  `prf_adress` varchar(50) DEFAULT NULL,
+  `id_ville` int(20) DEFAULT NULL,
   `prf_isValidated` int(11) DEFAULT NULL,
   `prf_date_naissance` datetime DEFAULT NULL,
-  `prf_sexe` varchar(1) DEFAULT NULL,
-  `prf_img` varchar(70) DEFAULT NULL,
-  `id_typeuser` int(11) NOT NULL,
+  `prf_sexe` varchar(10) DEFAULT NULL,
   `prf_NumSIRN` varchar(40) DEFAULT NULL,
   `prf_nameofcompany` varchar(70) DEFAULT NULL,
   `prf_siteweb` varchar(70) DEFAULT NULL,
-  `prf_activity` varchar(70) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `prf_activity` varchar(20) DEFAULT NULL,
+  `prf_statut` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Déchargement des données de la table `prf_profiles`
+--
+
+INSERT INTO `prf_profiles` (`prf_id`, `prf_first_name`, `prf_last_name`, `prf_email`, `prf_password`, `id_typeuser`, `prf_logo`, `prf_phone`, `prf_adress`, `id_ville`, `prf_isValidated`, `prf_date_naissance`, `prf_sexe`, `prf_NumSIRN`, `prf_nameofcompany`, `prf_siteweb`, `prf_activity`, `prf_statut`) VALUES
+(1, 'souzane', 'janson', 'souzane@gmail.com', 'souzane', 'Particulier', '\r\n', '+216 95478126', 'paris', 0, NULL, '2021-09-09 00:00:00', 'femme', NULL, NULL, NULL, 'étudiante', 0),
+(2, 'test1', 'testeur1', 'test@test.com', 'test', 'Particulier', '\r\n', '+216 95478126', 'paris', 0, NULL, '2021-09-29 00:00:00', 'homme', NULL, NULL, NULL, 'étudiante', 0),
+(3, 'testeur1', 'test1', 'test1@test.com', 'test1', 'Agence', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0);
 -- --------------------------------------------------------
 
 --
